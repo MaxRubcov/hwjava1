f19cb1f76b7212f5f2e2040c7b91ef6f70844a9e
MaxRU <m_rubcov@bk.ru>
2024-01-30 23:36:28 +0300

    added commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int price = 8363; // цена билета
-        int x = 20; //количество рублей для одной бонусной мили
-        int bonus = price / x; // количество начисленных миль
+        int perMile = 20; //количество рублей для одной бонусной мили
+        int bonus = price / perMile; // количество начисленных миль
         System.out.println(bonus);
     }
 }
